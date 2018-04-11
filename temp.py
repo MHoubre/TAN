359623bd46fc8192c77cb45be11a9cbb1d000603
Maël Houbre <maelhoubre@outlook.fr>
2018-04-11 21:22:00 +0200

    etapes projet

--- temp.py
+++ temp.py
@@ -7,7 +7,7 @@ Ceci est un script temporaire.
 
 import matplotlib.pyplot as plt
 import numpy as np
-#import wave
+import wave
 import math
 import binascii
 import pygame
@@ -26,6 +26,7 @@ datap=data[0:taille]
 
 t=np.arange(len(datap))/rate
 
+bruit=np.random.normal(0,0.5,len(datap))
 #print(len(t))
 
 tfd= fft(datap)
@@ -35,20 +36,20 @@ print(len(tfd))
 plt.subplot(211)
 plt.plot(t,datap)
 
-spectre=np.absolute(tfd)*2/n
+plt.subplot(212)
+plt.plot(t,bruit)
+
+sig_bruit = datap+bruit
+
+tfd2= fft(sig_bruit)
+
+spectre=np.absolute(tfd2)*2/taille
 freq=np.arange(taille)*1.0/5
 
 #plt.plot(freq,spectre)
 
 #plt.figure(figsize=(30,4))
-for i in range(0,taille-2,2):
-    tfd[i]=tfd[i]-50000
 
-itfd=ifft(tfd)
-plt.subplot(212)
-plt.plot(t,itfd)
-#type(data[3])
-
-#plt.plot(t,y)
-#grid()
-#plt.show()
+itfd=ifft(tfd2)
+#plt.subplot(212)
+#plt.plot(t,itfd)
