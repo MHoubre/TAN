d3db5b1e960c968aa08925c3b5f3e963e59ca3de
Ododo <olivierdautricourt@gmail.com>
2018-05-10 20:17:14 +0200

    instant?

--- rf_conduit.py
+++ rf_conduit.py
@@ -6,6 +6,10 @@ import sounddevice as sd
 from matplotlib import pyplot as plt
 from reconstruct import reconst
 
+
+N=2048
+soundarray = None
+
 #
 # def tf_short_term(data, N):
 #     hw = sig.hanning(N)
@@ -44,26 +48,53 @@ def apply_vocal_spectrum(E, H):
 
     return V
 
+def apply_transformation(voice, sound):
+    data = voice[0:len(sound)]
+    sound = sound[0:len(data)]
+
+    fv, tv, zv = sig.stft(voice, nperseg=N)
+    fs, ts, zs = sig.stft(sound, nperseg=N)
+
+    H = vocal_frequency_response_estimation(zv)
+    V = apply_vocal_spectrum(zs, H)
+
+    t, R = sig.istft(V)
+
+    return R
+
+def callback(indata, outdata, frames, time, status):
+    if status:
+        print(status)
+    print(len(indata))
+    print(indata)
+    outdata[:] = indata
+    #outdata[:] = apply_transformation(indata, soundarray)
+
+
 
 if __name__ == "__main__":
+     fe, soundarray = wave.read('test_voix.wav')
+     print(fe)
 
-    fe, data = wave.read('test_voix.wav')
-    fe2, la = wave.read('0838.wav')
-#    print(n, fe)
-    la = la[0:len(data)]
-    data = data[0:len(la)]
-    f, t, Zxx = sig.stft(data, nperseg=2048) #partie 1 :/
-    f2, t2, E = sig.stft(la, nperseg=2048) 
+     with sd.Stream(channels=1, callback=callback, blocksize=N, device=(0, 9)):
+         sd.sleep(int(20 * 1000))
 
-    H = vocal_frequency_response_estimation(Zxx)
-
-    V = apply_vocal_spectrum(E, H)
-
-    t, R = sig.istft(V) # partie reconstruction :/
-
-    R = R / 100000 #On devra peut etre appliquer un facteur 
-
-    wave.write("chasse-eau-baptiste.wav", fe, R)
+#     fe2, la = wave.read('0838.wav')
+# #    print(n, fe)
+#     la = la[0:len(data)]
+#     data = data[0:len(la)]
+#     f, t, Zxx = sig.stft(data, nperseg=2048) #partie 1 :/
+#     f2, t2, E = sig.stft(la, nperseg=2048)
+#
+#     H = vocal_frequency_response_estimation(Zxx)
+#
+#     V = apply_vocal_spectrum(E, H)
+#
+#     t, R = sig.istft(V) # partie reconstruction :/
+#
+#     R = R / 100000 #On devra peut etre appliquer un facteur
+#
+#     wave.write("chasse-eau-baptiste.wav", fe, R)
     # plt.plot(data)
     # plt.figure()
     # plt.plot(H[10])
